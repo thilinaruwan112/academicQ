ded218472816d78cd934cff33ee4467d7b31b5d6
Thilina Ruwan <thilinaruwan112@gmail.com>
2025-08-09 13:40:29 +0000

    use full width of the right side

--- src/app/(app)/layout.tsx
+++ src/app/(app)/layout.tsx
@@ -88,7 +88,7 @@ export default function AppLayout({
           </SidebarFooter>
         </Sidebar>
         <SidebarInset>
-          <div className="min-h-screen bg-background p-4 sm:p-6 lg:p-8">
+          <div className="min-h-screen w-full bg-background p-4 sm:p-6 lg:p-8">
             {children}
           </div>
         </SidebarInset>
